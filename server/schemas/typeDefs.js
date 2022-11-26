2e72cea51c763ca3c8cbce1cbd0a089948000700
nofutofu <aaronrweis@gmail.com>
2022-11-25 18:14:12 -0600

    typedefs for graphql queries

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,47 @@
+const { gql } = require ('apollo-server-express');
+
+const typeDefs = gql`
+  input saveBookInput {
+    authors: String
+    description: String!
+    title: String!
+    bookId: String!
+    image: String
+    link: String
+  }
+
+  type Query {
+    me: User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(user: String!, email: String!, password: String!): Auth
+    saveBook(input: saveBookInput): User
+    removeBook(bookId: ID!): User
+  }
+
+  type User {
+    _id: ID
+    username: String!
+    email: String!
+    bookCount: Int
+    savedBooks: [Book]!
+  }
+
+  type Book {
+    bookId: String!
+    authors [String]
+    description: String!
+    title: String!
+    image: String
+    link: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+`;
+
+module.exports = typeDefs;
